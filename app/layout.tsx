ee41e4a75445b55a398b11c774e43a8ea8d642e5
Benny Gächter <benny.gaechter@cloudreach.com>
2024-02-21 10:52:46 +0100

    finish tutorial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import "@aws-amplify/ui-react/styles.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+import ConfigureAmplifyClientSide from "@/components/ConfigureAmplify";
 
 const inter = Inter({ subsets: ['latin'] })
 
